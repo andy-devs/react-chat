c3d3ef1f86d39041b215e56e9866aa72bb68a57a
andy-devs <markushin.andrew@gmail.com>
2022-05-01 20:37:47 +0600

    render messages

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,21 +1,41 @@
 import { useState, useEffect } from 'react';
 import { db } from '../firebase';
-import { collection, doc, setDoc } from 'firebase/firestore';
+import {
+	collection,
+	getDocs,
+	query,
+	orderBy,
+	limit,
+	onSnapshot,
+} from 'firebase/firestore';
 import SignOut from './SignOut';
+import classes from './Chat.module.css';
 
 const Chat = () => {
-	const [messages, setMessages] = useState();
+	const [messages, setMessages] = useState<any[]>([]);
 
-	useEffect(() => {
-		const messages = collection(db, 'messages');
-		console.log(messages);
-	}, []);
+	const fetchMessages = async () => {
+		const messagesRef = collection(db, 'messages');
+		const messagesQuery = query(messagesRef, orderBy('createdAt'), limit(50));
+		onSnapshot(messagesQuery, (snapshot) => {
+			setMessages(snapshot.docs.map((doc) => doc.data()));
+		});
+	};
+
+	fetchMessages();
 
 	return (
-		<div>
+		<main className={classes.chat}>
 			<h1>Chat</h1>
+			<ul>
+				{messages.map((message: any) => (
+					<li>
+						{message.author} - {message.text}
+					</li>
+				))}
+			</ul>
 			<SignOut />
-		</div>
+		</main>
 	);
 };
 export default Chat;
